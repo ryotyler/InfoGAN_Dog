27efdb7f62371b17d65ce46b73dd89986f2fefaa
Tyler Shibata <yoyo2000@stanford.edu>
2021-12-02 10:04:57 -0800

    reverted dog model

--- models/stanforddog_model.py
+++ models/stanforddog_model.py
@@ -14,10 +14,10 @@ class Generator(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.tconv1 = nn.ConvTranspose2d(74, 1024, 1, 1, bias=False)
+        self.tconv1 = nn.ConvTranspose2d(74, 1024, 2, 1, bias=False)
         self.bn1 = nn.BatchNorm2d(1024)
 
-        self.tconv2 = nn.ConvTranspose2d(1024, 128, 7, 1, bias=False)
+        self.tconv2 = nn.ConvTranspose2d(1024, 128, 7, 2, bias=False)
         self.bn2 = nn.BatchNorm2d(128)
 
         self.tconv3 = nn.ConvTranspose2d(128, 64, 4, 2, padding=1, bias=False)
