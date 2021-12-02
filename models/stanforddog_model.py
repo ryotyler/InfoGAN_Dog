5e96c64d53b27914c3c76f3c34d73d34e9a89856
Tyler Shibata <yoyo2000@stanford.edu>
2021-12-02 08:53:58 -0800

    altered dog model

--- models/stanforddog_model.py
+++ models/stanforddog_model.py
@@ -67,7 +67,7 @@ class DHead(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.conv = nn.Conv2d(256, 1, 1)
+        self.conv = nn.Conv2d(128, 1, 1)
 
     def forward(self, x):
         output = torch.sigmoid(self.conv(x))
@@ -78,7 +78,7 @@ class QHead(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.conv1 = nn.Conv2d(256, 128, 1, bias=False)
+        self.conv1 = nn.Conv2d(128, 128, 1, bias=False)
         self.bn1 = nn.BatchNorm2d(128)
 
         self.conv_disc = nn.Conv2d(128, 10, 1)
