1492f6897e181a5a0cf9c3f49aa199ba02c9c11c
Tyler Shibata <yoyo2000@stanford.edu>
2021-12-02 10:55:48 -0800

    print some shit

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ params = {
     'beta1': 0.5,
     'beta2': 0.999,
     'save_epoch' : 25,# After how many epochs to save checkpoints and generate test output.
-    'dataset' : 'MNIST'}# Dataset to use. Choose from {MNIST, StanfordDog, CelebA, FashionMNIST}. CASE MUST MATCH EXACTLY!!!!!
+    'dataset' : 'StanfordDgo'}# Dataset to use. Choose from {MNIST, StanfordDog, CelebA, FashionMNIST}. CASE MUST MATCH EXACTLY!!!!!
--- train.py
+++ train.py
@@ -188,6 +188,9 @@ for epoch in range(params['num_epochs']):
         # Calculating loss for discrete latent code.
         dis_loss = 0
         for j in range(params['num_dis_c']):
+            print(q_logits.size())
+            print(target.size())
+            print(urmom)
             dis_loss += criterionQ_dis(q_logits[:, j*10 : j*10 + 10], target[j])
 
         # Calculating loss for continuous latent code.
