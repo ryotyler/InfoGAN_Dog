29b78630bcf6ef2eb03f8171b92397bd9dce8825
Tyler Shibata <yoyo2000@stanford.edu>
2021-12-02 19:05:46 -0800

    params update

--- models/stanforddog_model.py
+++ models/stanforddog_model.py
@@ -125,8 +125,8 @@ class QHead(nn.Module):
         self.bn1 = nn.BatchNorm2d(128)
 
         self.conv_disc = nn.Conv2d(128, 100, 1)
-        self.conv_mu = nn.Conv2d(128, 3, 1)
-        self.conv_var = nn.Conv2d(128, 3, 1)
+        self.conv_mu = nn.Conv2d(128, 1, 1)
+        self.conv_var = nn.Conv2d(128, 1, 1)
 
     def forward(self, x):
         x = F.leaky_relu(self.bn1(self.conv1(x)), 0.1, inplace=True)
